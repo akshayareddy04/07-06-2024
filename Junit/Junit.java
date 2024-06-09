56c6b412c8d8344b115e46435c0683a059e86aef
akshayareddy04 <akshayareddybodgam@gmail.com>
2024-06-09 16:52:35 +0530

    Create Junit.java

---
+++ Junit/Junit.java
@@ -0,0 +1,40 @@
+package Junit;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JunitTest {
+	@BeforeClass
+	public static void setUpClass(){
+		
+	}
+	@Before 
+	public void setUp() {
+		
+	}
+	@Test
+	public void test() {
+		Snippet snippet=new Snippet();
+		int number1=100;
+		int number2=5;
+		int actualValue=snippet.product(number1,number2);
+		int expectedValue=500;
+		assertEquals(expectedValue,actualValue);
+	}
+	@Test
+	public void division() {
+		
+	}
+	@After
+	public void tearDown() {
+		
+	}
+	@AfterClass
+	public static void tearDownClass() {
+		
+	}
+}
